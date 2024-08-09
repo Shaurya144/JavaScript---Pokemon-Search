24d5878fd4038dc442f2618b457873b571c9feae
Shaurya144 <chundawatshaurya@gmail.com>
2024-08-09 15:11:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const SearchInput = document.getElementById("search-input");
 const SearchButton = document.getElementById("search-button");
-
 const PokemonName = document.getElementById("pokemon-name");
 const PokemonID = document.getElementById("pokemon-id");
 const PokemonWeight = document.getElementById("weight");
@@ -22,8 +21,43 @@ const fetchData = async () => {
     const res = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemonNameOrID}`);
     const data = await res.json();
 
+    setPokemonInfo(data);
+
   } catch(err) {
-    alert("Pokemon not found")
-    console.log(err)
+    alert("Pokémon not found");
+    console.log(err);
   }
-}
+};
+
+const setPokemonInfo = data => {
+  const {name, id, weight, height, types, sprites, stats} = data; // this is how you destructure the object
+
+  PokemonName.textContent = `${name[0].toUpperCase() + name.slice(1)}`;
+  PokemonID.textContent = `#${id}`;
+  PokemonWeight.textContent = `${weight}`;
+  PokemonHeight.textContent = `${height}`;
+
+  PokemonHealth.textContent = stats[0].base_stat;
+  PokemonAttack.textContent = stats[1].base_stat;
+  PokemonDefense.textContent = stats[2].base_stat;
+  PokemonSpecialAttack.textContent = stats[3].base_stat;
+  PokemonSpecialDefence.textContent = stats[4].base_stat;
+  PokemonSpeed.textContent = stats[5].base_stat;
+
+  // Sprite
+  PokemonSprite.innerHTML = `<img id="sprite" src="${sprites.front_default}" alt="${name}">`;
+
+  // Types
+  PokemonTypes.innerHTML = types.map(obj =>
+    ` 
+    <span>${obj.type.name[0].toUpperCase() + obj.type.name.slice(1)}</span>
+    `
+  ).join(" ");
+  
+};
+
+// Our Event Listeners
+SearchButton.addEventListener("click", e => {
+  e.preventDefault();
+  fetchData();
+});
